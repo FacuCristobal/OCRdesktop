4506d86a49e4e38e6e85d171f25d35ea3b31d313
FacuCristobal <68520164+FacuCristobal@users.noreply.github.com>
2020-07-27 11:59:46 -0300

    limpia3
    organizado un tq

--- src/js/render.js
+++ src/js/render.js
@@ -1,60 +1,19 @@
+// TESSERACT //
+
 const { createWorker } = require('tesseract.js');
-var FileSaver = require('file-saver');
-
-var files = [];
-
-var traduccion = '';
-
-function transcribe(){
-  files.forEach(element => {
-    tese(element.path);
-  });
-}
 
 const worker = createWorker({
 //  logger: m => console.log(m), // Add logger here
   logger: m => showProgress(m),
 });
 
-function showProgress(m) {
+function showProgress(m) { //Barra de progreso y status
   $("#progbar").css("width", (m.progress * 100) + "%");
   $("#status")[0].innerHTML = m.status;
   console.log(m);
 };
 
-$("#inImage").change(function(){
-  var archivos = $("#inImage")[0].files;
-  var archivo = {};
-  for (let i = 0; i < archivos.length; i++) {
-    archivo.path = archivos[i].path;
-    archivo.name = archivos[i].name;
-    archivo.lang = 'spa';
-    console.log(archivo);
-    files[i] = archivos[i].path;
-  }
-  var imagen = new Image();
-  imagen.src = archivos[0].path;
-  mostrarImagen(imagen);
-  $("#status")[0].innerHTML = "listo pa' typear";
-});
-
-function mostrarImagen(img){
-  var oldImg = $("#suki img")[0];
-  if (oldImg) {
-    oldImg.remove();
-  }
-  
-  $("#suki").append(img);
-}
-
-
-
-$("#typear").click(function(){
-  $( "#texto" )[0].innerHTML = '';
-  readImage(files[0].path, files[0].lang, files[0].name);
-});
-
-function readImage(path){
+function readImage(path){ //Transcripcion tesseract
   (async () => {
     await worker.load();
     await worker.loadLanguage('eng');
@@ -70,22 +29,42 @@ function readImage(path){
     }
     
     $("#status")[0].innerHTML = "Completado";
-    // await worker.terminate();
+    // await worker.terminate(); // cuando se ejecuta el worker no transcribe mas
   })();
 };
 
-function tese(img) {
-  console.log("tese");
-  Tesseract.recognize(
-    img,
-    'spa',
-    { logger: m => console.log(m) }
-  ).then(({ data: { text } }) => {
-    console.log(text);
-  })
+
+// ------------------------------------------
+
+var files = [];
+
+var FileSaver = require('file-saver');
+
+var imagen = new Image();
+
+$("#inImage").change(function(){ // Carga los archivos en la variable files y muestra la primer imagen
+  var archivos = $("#inImage")[0].files;
+  var archivo = {};
+  for (let i = 0; i < archivos.length; i++) {
+    archivo.path = archivos[i].path;
+    archivo.name = archivos[i].name;
+    archivo.lang = 'spa';
+    console.log(archivo);
+    files[i] = archivos[i].path;
+  }
+  imagen.src = archivos[0].path;
+  mostrarImagen(imagen);
+  $("#status")[0].innerHTML = "listo pa' typear";
+});
+
+function mostrarImagen(img){
+  var oldImg = $("#suki img")[0];
+  if (oldImg) {
+    oldImg.remove();
+  }
+  
+  $("#suki").append(img);
 }
-
-
 
 $( ".divImg" ).hover(function() {
   $( "#botong" ).fadeToggle();
@@ -95,7 +74,7 @@ function() {
 }
 );
 
-function selectText(tere) {
+function selectText() {
   if (document.selection) {
       let div = document.body.createTextRange();
 
@@ -154,13 +133,26 @@ dropzone.on('drop',function(e) {
   //console.log(e);
 //}, false);
 
-$('#suki').click (function(){
-  $('#inImage')[0].click();
-} ); 
+
 
 // FILESAVER
 
 var blob = new Blob([$("#texto")[0].value], {type: "text/plain;charset=utf-8"});
 //FileSaver.saveAs(blob, "transcripccion.txt");
 
-$("#guarderia").click(() => FileSaver.saveAs(blob, "transcripccion.txt"));
+$("#guarderia").click(() => FileSaver.saveAs(blob, "transcripcion.txt"));
+
+
+// BOTONES EVENTOS
+$("#typear").click(function(){ // Evento Tipear
+  $( "#texto" )[0].innerHTML = '';
+  readImage(files[0].path, files[0].lang, files[0].name);
+});
+
+$('#suki').click (function(){ // cuando se hace click en cualquier lugar de suki se dispara el evento click en input file
+  $('#inImage')[0].click();
+} ); 
+
+$("#selec").click(function (){ // Copia el texto al portapapeles
+  selectText()
+});
